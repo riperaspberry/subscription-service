7ebbccda3b08b8eaf5998ba8ae1ef57aa20a2c6c
Karina <kvanolik@yandex.ru>
2026-07-23 15:58:57 +0300

    Add subscription models

---
+++ internal/model/subscriptions.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Subscription struct {
+	ID          uuid.UUID `json:"id"`
+	ServiceName string    `json:"service_name"`
+	Price       int       `json:"price"`
+	UserID      uuid.UUID `json:"user_id"`
+	StartDate   time.Time `json:"start_date"`
+	EndDate     *time.Time `json:"end_date,omitempty"`
+	CreatedAt   time.Time `json:"created_at"`
+}
